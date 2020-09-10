3a5c384bbcd06c18ea9dc13f5c3c518d50d5f381
Annastasshia Ames <aames26@gmail.com>
2020-09-10 19:48:32 -0400

    book search linked / map results of image links

--- google/src/App.js
+++ google/src/App.js
@@ -1,12 +1,13 @@
 import React, {useState}from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   
   const [book,setBook] = useState("");
   const [result,setResult] = useState([]);
-  const [apiKey,setApiKey] = useState("");
+  const [apiKey,setApiKey] = useState("AIzaSyCot_cw0iLNMHYqJmw-ZrYRPGi_1ey1uXg");
 
   function handleChange(event){
     console.log(this)
@@ -15,8 +16,15 @@ function App() {
   };
 
   function handleSubmit(event){
+    event.preventDefault();
+
     console.log("form submitted")
 
+    axios.get("https://www.googleapis.com/books/v1/volumes?q="+ book +"&key=" + apiKey)
+    .then(data => {
+      console.log(data.data.items);
+      setResult(data.data.items);
+    })
   }
 
   return (
@@ -33,6 +41,11 @@ function App() {
           <input type="text" onChange= {handleChange} placeholder="Search for Books..." autoComplete="off"/>
         <button>Search</button>
         </form>
+        <card>
+        {result.map(book => (
+          <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.title}/>
+        ))}
+        </card>
       </div>
     </div>
   );
